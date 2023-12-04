e36197543ee5e4069dcafe7516e6445df1eaca24
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-04 22:04:18 +0000

    Solve 1.1

--- src/Day01.purs
+++ src/Day01.purs
@@ -4,16 +4,26 @@ import Prelude
 import Utils.String (lines)
 import Data.Traversable (traverse)
 import Parsing (ParseError, runParser)
-import Parsing.Combinators.Array (many) as P
+import Parsing.Combinators.Array (many1) as P
 import Parsing.String (anyTill) as P
 import Data.Tuple (snd) as Tuple
 import Data.Either (Either)
 import Utils.Parsing (intDigit)
+import Utils.Pointfree ((<<$>>))
+import Data.Array as Array
+import Data.Foldable (sum) as F
+import Data.Array.NonEmpty (NonEmptyArray)
+import Data.Array.NonEmpty as NA
 
-parse :: String -> Either ParseError (Array (Array Int))
+solve1 :: String -> Either ParseError Int
+solve1 = F.sum <<< map calibration <<$>> parse
+  where
+    calibration digits = (NA.head digits * 10) + (NA.last digits)
+
+parse :: String -> Either ParseError (Array (NonEmptyArray Int))
 parse = lines >>> traverse (flip runParser digits)
   where
-    digits = P.many $ Tuple.snd <$> P.anyTill intDigit
+    digits = P.many1 $ Tuple.snd <$> P.anyTill intDigit
 
 example1 = """1abc2
 pqr3stu8vwx
