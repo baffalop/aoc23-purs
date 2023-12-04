b3b416fe212ab4ce8c276603733a56c9772ebcaf
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-04 22:27:47 +0000

    Solve 1.2

--- src/Day01.purs
+++ src/Day01.purs
@@ -3,9 +3,9 @@ module Day01 where
 import Prelude
 import Utils.String (lines)
 import Data.Traversable (traverse)
-import Parsing (ParseError, runParser)
+import Parsing (ParseError, Parser, runParser)
 import Parsing.Combinators.Array (many1) as P
-import Parsing.String (anyTill) as P
+import Parsing.String (anyTill, string, takeN) as P
 import Data.Tuple (snd) as Tuple
 import Data.Either (Either)
 import Utils.Parsing (intDigit)
@@ -15,16 +15,43 @@ import Data.Foldable (sum) as F
 import Data.Array.NonEmpty (NonEmptyArray)
 import Data.Array.NonEmpty as NA
 import Input (readInput)
+import Parsing.Combinators (choice, lookAhead) as P
+import Control.Alt ((<|>))
+
+type Parse = String -> Either ParseError (Array (NonEmptyArray Int))
 
 solve1 :: String -> Either ParseError Int
-solve1 = F.sum <<< map calibration <<$>> parse
+solve1 = solve parse1
+
+solve2 :: String -> Either ParseError Int
+solve2 = solve parse2
+
+solve :: Parse -> String -> Either ParseError Int
+solve parse = F.sum <<< map calibration <<$>> parse
   where
     calibration digits = (NA.head digits * 10) + (NA.last digits)
 
-parse :: String -> Either ParseError (Array (NonEmptyArray Int))
-parse = lines >>> traverse (flip runParser digits)
+parse1 :: Parse
+parse1 = parseWith intDigit
+
+parse2 :: Parse
+parse2 = parseWith $ P.lookAhead (intDigit <|> wordDigit) <* P.takeN 1
   where
-    digits = P.many1 $ Tuple.snd <$> P.anyTill intDigit
+    wordDigit = P.choice
+      [ 1 <$ P.string "one"
+      , 2 <$ P.string "two"
+      , 3 <$ P.string "three"
+      , 4 <$ P.string "four"
+      , 5 <$ P.string "five"
+      , 6 <$ P.string "six"
+      , 7 <$ P.string "seven"
+      , 8 <$ P.string "eight"
+      , 9 <$ P.string "nine"
+      ]
+
+parseWith :: Parser String Int -> Parse
+parseWith digit =
+  lines >>> traverse (flip runParser $ P.many1 $ Tuple.snd <$> P.anyTill digit)
 
 example1 = """1abc2
 pqr3stu8vwx
