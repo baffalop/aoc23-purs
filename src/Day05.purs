df1653dd22ca9a92ffc6fdf847b12423d1d95439
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-05 22:42:45 +0000

    Solve 5.2!

--- src/Day05.purs
+++ src/Day05.purs
@@ -5,7 +5,7 @@ import Input (readInput)
 import Parsing (ParseError, parseErrorMessage, runParser)
 import Parsing.String (char, string) as P
 import Parsing.Combinators (sepBy, sepBy1, sepEndBy) as P
-import Data.List (List)
+import Data.List (List(..), (:))
 import Data.List.NonEmpty (NonEmptyList)
 import Data.List.NonEmpty as NL
 import Data.Map (Map)
@@ -16,7 +16,7 @@ import Data.Tuple (Tuple(Tuple))
 import Data.Traversable (traverse)
 import Data.Either (either, note) as Either
 import Data.Foldable as F
-import Data.Maybe (fromMaybe)
+import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Semigroup.Foldable (minimum) as NF
 import Data.BigInt (BigInt)
 import Data.BigInt as BigInt
@@ -28,27 +28,58 @@ type Almanac =
 
 type Mapping =
   { kind :: String
-  , ranges :: List Range
+  , translations :: List Translation
+  }
+
+type Translation =
+  { range :: Range
+  , offset :: BigInt
   }
 
 type Range =
   { from :: BigInt
   , to :: BigInt
-  , offset :: BigInt
   }
 
+solve1 :: String -> Either String BigInt
 solve1 s = do
   { seeds, mappings } <- mapLeft parseErrorMessage $ parse s
-  let
-    translate { kind, id } = do
-      { kind, ranges } <- Either.note ("No mapping from: " <> kind) $ Map.lookup kind mappings
-      let
-        offset = fromMaybe (BigInt.fromInt 0)
-          $ _.offset <$> F.find (\{ from, to } -> id >= from && id <= to) ranges
-        translated = id + offset
-      if kind == "location" then pure translated else translate { kind, id: translated }
+  NF.minimum <$> traverse (translateWith mappings translate) seeds
+  where
+    translate translations id =
+      F.find (\{ range: { from, to } } -> id >= from && id <= to) translations
+        <#> _.offset
+        # fromMaybe (BigInt.fromInt 0)
+        # (id + _)
 
-  NF.minimum <$> traverse (translate <<< { kind: "seed", id: _ }) seeds
+solve2 :: String -> Either String BigInt
+solve2 s = do
+  { seeds, mappings } <- mapLeft parseErrorMessage $ parse s
+  translated <- translateWith mappings translate $ toRanges $ NL.toList seeds
+  Either.note "no seeds"
+    $ F.minimum $ _.from <$> translated
+  where
+    toRanges (start : length : rest) = rangeFrom start length : toRanges rest
+    toRanges _ = Nil
+
+    translate :: List Translation -> List Range -> List Range
+    translate translations ranges = ranges >>= translateBy translations
+
+    translateBy :: List Translation -> Range -> List Range
+    translateBy Nil range = range : Nil
+    translateBy ({ range: transRange, offset } : translations) range =
+        let { within, remainder } = intersect range transRange
+        in (bump offset <$> within) ?: translate translations remainder
+
+    bump offset { from, to } = { from: from + offset, to: to + offset }
+
+translateWith :: forall a. Map String Mapping -> (List Translation -> a -> a) -> a -> Either String a
+translateWith mappings translate = doTranslate "seed"
+  where
+    doTranslate kind subject = do
+      { kind: toKind, translations } <- Either.note ("No mapping from: " <> kind) $ Map.lookup kind mappings
+      let translated = translate translations subject
+      if toKind == "location" then pure translated else doTranslate toKind translated
 
 parse :: String -> Either ParseError Almanac
 parse = flip runParser do
@@ -61,18 +92,43 @@ parse = flip runParser do
       source <- P.word
       destination <- P.string "-to-" *> P.word
       _ <- P.string " map:\n"
-      ranges <- range `P.sepEndBy` P.char '\n'
-      pure $ Tuple source { kind: destination, ranges }
+      translations <- translation `P.sepEndBy` P.char '\n'
+      pure $ Tuple source { kind: destination, translations }
 
-    range = do
+    translation = do
       target <- P.bigInt <* P.char ' '
-      from <- P.bigInt <* P.char ' '
+      start <- P.bigInt <* P.char ' '
       length <- P.bigInt
       pure
-        { from
-        , to: from + length - BigInt.fromInt 1
-        , offset: target - from
+        { range: rangeFrom start length
+        , offset: target - start
         }
+
+intersect :: Range -> Range -> { within :: Maybe Range, remainder :: List Range }
+intersect range target =
+  { within: validate
+    { from: max range.from target.from
+    , to: min range.to target.to
+    }
+  , remainder:
+    validate range { to = min range.to $ target.from - BigInt.fromInt 1 }
+    ?: validate range { from = max range.from $ target.to + BigInt.fromInt 1 }
+    ?: Nil
+  }
+  where
+    validate range'@{ from, to } = if to < from then Nothing else Just range'
+
+rangeFrom :: BigInt -> BigInt -> Range
+rangeFrom start length =
+  { from: start
+  , to: start + length - BigInt.fromInt 1
+  }
+
+maybeCons :: forall a. Maybe a -> List a -> List a
+maybeCons Nothing = identity
+maybeCons (Just x) = (x : _)
+
+infixr 6 maybeCons as ?:
 
 mapLeft :: forall a b c. (a -> b) -> Either a c -> Either b c
 mapLeft f = Either.either (Left <<< f) (Right <<< identity)
