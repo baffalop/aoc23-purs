ff75f3ed81ed4ad408eddc8e5f3993bfdcaabcca
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-06 13:47:18 +0000

    Solve 6.1

--- src/Day06.purs
+++ src/Day06.purs
@@ -10,11 +10,21 @@ import Parsing.Combinators (sepBy)
 import Data.List as List
 import Data.Either (Either)
 import Data.List (List)
+import Utils.Pointfree ((<<#>>))
+import Data.Foldable (product) as F
 
 type Race =
   { time :: Int
   , distance :: Int
   }
+
+solve1 :: String -> Either ParseError Int
+solve1 = parse <<#>> map countMoves >>> F.product
+  where
+    countMoves race@{ time } = time - (firstWinningMove race * 2) + 1
+
+    firstWinningMove { time, distance } = firstFrom 1
+      where firstFrom n = if n * (time - n) > distance then n else firstFrom $ n + 1
 
 parse :: String -> Either ParseError (List Race)
 parse s = runParser s do
