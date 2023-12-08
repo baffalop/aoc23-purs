ef66662cfb1d30cd32257d5506a08771974f3e31
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-08 11:40:54 +0000

    Attempt 8.1: stack overflow

--- src/Day08.purs
+++ src/Day08.purs
@@ -2,20 +2,24 @@ module Day08 where
 
 import Prelude
 import Input (readInput)
-import Parsing (ParseError, Parser, runParser)
+import Parsing (ParseError, Parser, parseErrorMessage, runParser)
 import Data.Map as Map
 import Data.Map (Map)
 import Data.Tuple (Tuple(..))
-import Parsing.Combinators.Array (many) as P
+import Parsing.Combinators (many) as P
 import Parsing.String (char, string) as P
 import Control.Alt ((<|>))
 import Utils.Parsing (linesOf, word) as P
 import Data.Either (Either)
+import Data.List as List
+import Data.List (List(..), (:))
+import Data.Either (note) as Either
+import Data.Bifunctor (lmap)
 
 data Step = L | R
 
 type Navigation =
-  { steps :: Array Step
+  { steps :: List Step
   , network :: Network
   }
 
@@ -24,6 +28,22 @@ type Network = Map String (Tuple String String)
 instance showStep :: Show Step where
   show L = "L"
   show R = "R"
+
+--solve1 :: String -> Either ParseError
+solve1 s = do
+  { steps, network } <- lmap parseErrorMessage $ parse s
+  let
+    navigate :: String -> List Step -> Int -> Either String Int
+    navigate "ZZZ" _ n = pure n
+    navigate loc Nil n = navigate loc steps n
+    navigate loc (step : restSteps) n = do
+      Tuple left right <- Either.note ("location not found: " <> loc) $ Map.lookup loc network
+      let nextLoc = case step of
+            L -> left
+            R -> right
+      navigate nextLoc restSteps (n + 1)
+
+  navigate "AAA" steps 0
 
 parse :: String -> Either ParseError Navigation
 parse s = runParser s do
