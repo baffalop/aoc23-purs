ba093d4840d406027f34eaac168d2d482aeca7a7
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-09 10:05:14 +0000

    Solve 9.1

---
+++ src/Day09.purs
@@ -0,0 +1,33 @@
+module Day09 where
+
+import Prelude
+import Utils.String (lines, words) as String
+import Data.Traversable (traverse)
+import Data.Int (fromString) as Int
+import Data.Maybe (Maybe)
+import Data.Array as Array
+import Utils.Pointfree ((<<#>>))
+import Data.Foldable as F
+import Data.Array ((:))
+import Input (readInput)
+
+solve1 :: String -> Maybe Int
+solve1 = parse
+  <<#>> map (diffTo0 >>> Array.mapMaybe Array.last >>> F.sum)
+  >>> F.sum
+  where
+    diffTo0 xs =
+      let deriv = derivative xs
+      in if F.all (_ == 0) deriv then [xs] else xs : diffTo0 deriv
+
+parse :: String -> Maybe (Array (Array Int))
+parse = String.lines >>> traverse (String.words >>> traverse Int.fromString)
+
+derivative :: forall a. Ring a => Array a -> Array a
+derivative xs = Array.zipWith (-) (Array.drop 1 xs) xs
+
+example = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+
+input = readInput 9
--- src/Utils/String.purs
+++ src/Utils/String.purs
@@ -3,6 +3,22 @@ module Utils.String where
 import Prelude
 import Data.String as S
 import Data.String.Pattern (Pattern(Pattern))
+import Data.String.Regex as R
+import Data.String.Regex (Regex)
+import Data.String.Regex.Flags (noFlags) as RF
+import Partial.Unsafe (unsafeCrashWith)
+import Data.String.Regex.Flags (RegexFlags)
+import PointFree ((<..))
+import Data.Either (either)
 
 lines :: String -> Array String
 lines = S.split (Pattern "\n")
+
+words :: String -> Array String
+words = R.split whitespaceRegex
+
+whitespaceRegex :: Regex
+whitespaceRegex = unsafeRegex "\\s+" RF.noFlags
+
+unsafeRegex :: String -> RegexFlags -> Regex
+unsafeRegex = either unsafeCrashWith identity <.. R.regex
