5d3f3596bcda5aa9ee5a84194b446bad980fbc37
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-10 12:58:59 +0000

    Solve 10.1

--- src/Day10.purs
+++ src/Day10.purs
@@ -2,24 +2,58 @@ module Day10 where
 -- Pipe Maze
 
 import Prelude
-import Parsing (ParseError, Position(Position), runParser)
+import Parsing (ParseError, Position(Position), parseErrorMessage, runParser)
 import Utils.Parsing (linesOf)
 import Data.Map (Map)
 import Data.Map as Map
 import Utils.Pointfree ((<<$>>))
 import Parsing.Combinators.Array (many) as P
 import Data.Tuple.Nested ((/\), type (/\))
-import Parsing.Combinators (choice) as P
+import Parsing.Combinators (choice, skipMany) as P
 import Parsing.String (char) as P
 import Parsing (position) as P
-import Data.Tuple (Tuple(Tuple))
+import Data.Tuple (Tuple(Tuple), fst, snd)
 import Control.Bind (join)
 import Data.Either (Either)
+import Data.Maybe (Maybe(..))
+import Data.Either (note) as Either
+import Data.Array as Array
+import Data.Bifunctor (lmap)
+import Data.Array.NonEmpty (NonEmptyArray, (:))
+import Data.Array.NonEmpty as NA
+import Data.Foldable as F
+import Control.Alt ((<|>))
+import Input (readInput)
+import Data.Foldable (class Foldable)
+import Control.Alternative (class Alternative)
+import Control.Plus (class Plus, empty)
 
 type Coord = Int /\ Int
 
+solve1 :: String -> Either String Int
+solve1 s = do
+  pipes <- lmap parseErrorMessage $ parse s
+  start <- Either.note "No start found" $ fst <<$>> Array.find (snd >>> Array.null) $ Map.toUnfoldable pipes
+  let
+    follow :: NonEmptyArray Coord -> Coord -> Maybe (NonEmptyArray Coord)
+    follow visited to
+      | to == start = Just visited
+      | otherwise = case Array.filter (_ /= NA.head visited) <$> Map.lookup to pipes of
+        Just [next] -> follow (to : visited) next
+        _ -> Nothing
+
+  route <- Either.note "No route found" $ oneOf $ follow (NA.singleton start) <$> neighbours start
+  pure $ NA.length route `div` 2
+
+oneOf :: forall f m a. Foldable f => Alternative m => f (m a) -> m a
+oneOf = F.foldr (<|>) empty
+
+neighbours :: Coord -> Array Coord
+neighbours c = (c + _) <$> [north, south, east, west]
+
 parse :: String -> Either ParseError (Map Coord (Array Coord))
 parse s = runParser s $ Map.fromFoldable <<< join <<$>> linesOf $ P.many do
+  dot
   openings <- P.choice
     [ [] <$ P.char 'S'
     , [north, south] <$ P.char '|'
@@ -30,8 +64,11 @@ parse s = runParser s $ Map.fromFoldable <<< join <<$>> linesOf $ P.many do
     , [east, north] <$ P.char 'L'
     ]
   Position { line, column } <- P.position
+  dot
   let coord = column /\ line
   pure $ Tuple coord $ (coord + _) <$> openings
+  where
+    dot = P.skipMany $ P.char '.'
 
 north = 0 /\ -1
 south = 0 /\ 1
@@ -43,3 +80,5 @@ example2 = """7-F7-
 SJLL7
 |F--J
 LJ.LJ"""
+
+input = readInput 10
