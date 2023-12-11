476720dde2113e7234706e35291cb9d5851533a6
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-11 16:12:46 +0000

    Fix double-confusion over rows and columns

--- src/Day11.purs
+++ src/Day11.purs
@@ -36,8 +36,8 @@ solveWith expansionFactor s =
     populatedCols = Set.fromFoldable $ fst <$> galaxies
     populatedRows = Set.fromFoldable $ snd <$> galaxies
     maxX /\ maxY = mapBoth (fromMaybe 0 <<< Set.findMax) $ populatedCols /\ populatedRows
-    emptyRows = Set.toMap $ Set.fromFoldable $ Array.filter (\x -> not $ Set.member x populatedRows) (0 .. maxX)
-    emptyCols = Set.toMap $ Set.fromFoldable $ Array.filter (\y -> not $ Set.member y populatedCols) (0 .. maxY)
+    emptyCols = Set.toMap $ Set.fromFoldable $ Array.filter (\x -> not $ Set.member x populatedCols) (0 .. maxX)
+    emptyRows = Set.toMap $ Set.fromFoldable $ Array.filter (\y -> not $ Set.member y populatedRows) (0 .. maxY)
     expandedGalaxies = bimap (expandBy emptyCols) (expandBy emptyRows) <$> galaxies
   in
   F.sum $ uncurry manhattanDistance <$> pairs expandedGalaxies
