0cbab1539cc04344d4c28bde2602382cffd9f658
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-13 23:00:26 +0000

    Attempt 13.1

--- src/Day13.purs
+++ src/Day13.purs
@@ -10,11 +10,20 @@ import Utils.Parsing (linesOf) as P
 import Data.Traversable (traverse)
 import Parsing.Combinators.Array (many) as P
 import Parsing.Combinators (choice) as P
-import Matrix as Matrix
+import Matrix as Mx
 import Matrix (Matrix)
-import Data.Maybe (maybe)
+import Data.Maybe (Maybe(..), fromMaybe, maybe)
 import Parsing (fail) as P
 import Data.Either (Either)
+import Utils.Pointfree ((<<#>>))
+import Data.Map.Internal (fromFoldableWith) as Map
+import Data.Array as Array
+import Data.Array ((..))
+import Data.Tuple (Tuple(Tuple))
+import Data.Tuple as Tuple
+import PointFree ((<..))
+import Data.Set as Set
+import Data.Foldable as F
 
 data Terrain = Ash | Rock
 
@@ -26,6 +35,31 @@ instance showTerrain :: Show Terrain where
 
 type Pattern = Matrix Terrain
 
+solve1 :: String -> Either ParseError Int
+solve1 = parse
+  <<#>> map (\p -> 100 * findReflection (Mx.rows p) + findReflection (Mx.columns p))
+  >>> F.sum
+
+findReflection :: Array (Array Terrain) -> Int
+findReflection slices = fromMaybe 0 $ F.findMap reflectionPoint reflectedPairs
+  where
+    bound = Array.length slices - 1
+
+    reflectedPairs = Array.mapWithIndex (\i s -> Tuple s [i]) slices
+      # Map.fromFoldableWith (Array.sort <.. (<>))
+      # (Array.fromFoldable >=> pairs)
+      # Set.fromFoldable
+
+    reflectionPoint (Tuple i1 i2)
+      | i1 == 0 || i2 == bound =
+        let refl = ((i2 - i1) `div` 2) + i1
+        in if F.all (\i -> (Tuple i $ i1 + i2 - i) `Set.member` reflectedPairs) ((i1 + 1) .. refl)
+          then Just $ refl + 1 else Nothing
+      | otherwise = Nothing
+
+    pairs :: forall a. Array a -> Array (Tuple a a)
+    pairs xs = join $ Array.mapWithIndex (\i x -> Tuple x <$> Array.drop (i + 1) xs) xs
+
 parse :: String -> Either ParseError (Array Pattern)
 parse = String.split (S.Pattern "\n\n") >>> traverse (flip runParser pattern)
   where
@@ -34,7 +68,7 @@ parse = String.split (S.Pattern "\n\n") >>> traverse (flip runParser pattern)
         [ Ash <$ P.char '.'
         , Rock <$ P.char '#'
         ]
-      maybe (P.fail "Can't build Matrix") pure $ Matrix.fromArray ar
+      maybe (P.fail "Can't build Matrix") pure $ Mx.fromArray ar
 
 example = """#.##..##.
 ..#.##.#.
