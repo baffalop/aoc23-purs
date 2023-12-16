18338d787aba27f5009ee7cbb2f844c02f7b76ee
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-16 13:16:41 +0000

    Solve 15.2

--- src/Day15.purs
+++ src/Day15.purs
@@ -15,6 +15,15 @@ import Parsing.String (char) as P
 import Utils.Parsing (word) as P
 import Parsing.Combinators (choice) as P
 import Parsing.String.Basic (intDecimal) as P
+import Utils.Pointfree ((<<#>>))
+import Data.Foldable (foldl)
+import Data.Map (Map)
+import Data.Map as Map
+import Data.Array as Array
+import Data.Maybe (Maybe(..), fromMaybe)
+import Control.Alt ((<|>))
+import Utils.Array ((<:))
+import Data.FoldableWithIndex (foldlWithIndex)
 
 type Instruction =
   { label :: String
@@ -27,8 +36,33 @@ instance showOp :: Show Op where
   show Remove = "Remove"
   show (Place f) = "Place " <> show f
 
+type Lens =
+  { label :: String
+  , focalLength :: Int
+  }
+
 solve1 :: String -> Int
 solve1 = String.split (Pattern ",") >>> map hash >>> F.sum
+
+solve2 :: String -> Either ParseError Int
+solve2 = parse <<#>> foldl doInstruction Map.empty >>> focusingPower
+  where
+    doInstruction :: Map Int (Array Lens) -> Instruction -> Map Int (Array Lens)
+    doInstruction boxes { label, op } =
+      let box = hash label
+      in case op of
+        Remove -> Map.update (Just <<< Array.filter (_.label >>> (_ /= label))) box boxes
+        Place focalLength -> Map.alter (Just <<< insertLens <<< fromMaybe []) box boxes
+          where
+            lens = { label, focalLength }
+            insertLens lenses = fromMaybe (lenses <: lens) $ updateLens lenses
+            updateLens lenses = do
+              i <- Array.findIndex (_.label >>> (_ == label)) lenses
+              Array.updateAt i lens lenses
+
+    focusingPower :: Map Int (Array Lens) -> Int
+    focusingPower = 0 # foldlWithIndex \box total lenses ->
+      total + foldlWithIndex (\i boxTotal { focalLength } -> boxTotal + (box + 1) * (i + 1) * focalLength) 0 lenses
 
 parse :: String -> Either ParseError (Array Instruction)
 parse s = runParser s $ instruction `arraySepBy` P.char ','
