5b1e85cda4fdd644a0eacb319713016c9a5bcd13
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-16 11:21:41 +0000

    Parse 15.2

--- src/Day15.purs
+++ src/Day15.purs
@@ -8,9 +8,35 @@ import Data.Char (toCharCode) as Char
 import Data.String.CodeUnits (toCharArray) as String
 import Data.Foldable as F
 import Input (readInput)
+import Data.Either (Either)
+import Parsing (ParseError, runParser)
+import Utils.Parsing (arraySepBy)
+import Parsing.String (char) as P
+import Utils.Parsing (word) as P
+import Parsing.Combinators (choice) as P
+import Parsing.String.Basic (intDecimal) as P
+
+type Instruction =
+  { label :: String
+  , op :: Op
+  }
+
+data Op = Remove | Place Int
+
+instance showOp :: Show Op where
+  show Remove = "Remove"
+  show (Place f) = "Place " <> show f
 
 solve1 :: String -> Int
 solve1 = String.split (Pattern ",") >>> map hash >>> F.sum
+
+parse :: String -> Either ParseError (Array Instruction)
+parse s = runParser s $ instruction `arraySepBy` P.char ','
+  where
+    instruction = { label: _, op: _} <$> P.word <*> P.choice
+      [ Remove <$ P.char '-'
+      , Place <$> (P.char '=' *> P.intDecimal)
+      ]
 
 hash :: String -> Int
 hash = String.toCharArray >>> F.foldl addHash 0
