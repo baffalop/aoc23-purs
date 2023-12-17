668985bc412c938cb6c9b65a6157b5e00e1beae8
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-17 11:02:59 +0000

    Day 13.2 solution finding both smudges and originals

--- src/Day13.purs
+++ src/Day13.purs
@@ -21,10 +21,12 @@ import Data.Map.Internal (fromFoldableWith) as Map
 import Data.Array as Array
 import Data.Tuple (Tuple(Tuple))
 import Data.Tuple as Tuple
+import Data.Tuple.Nested ((/\))
 import PointFree ((<..))
 import Data.Set as Set
 import Data.Foldable as F
-import Utils.Array ((../))
+import Data.Set (Set)
+import Data.Array ((..))
 
 data Terrain = Ash | Rock
 
@@ -36,30 +38,62 @@ instance showTerrain :: Show Terrain where
 
 type Pattern = Matrix Terrain
 
+invert Ash = Rock
+invert Rock = Ash
+
 solve1 :: String -> Either ParseError Int
 solve1 = parse
   <<#>> map (\p -> 100 * findReflection (Mx.rows p) + findReflection (Mx.columns p))
   >>> F.sum
 
+--solve2 :: String -> Either ParseError (array)
+solve2 = parse
+  <<#>> map (\pattern ->
+    case findSmudged (Mx.rows pattern) /\ findSmudged (Mx.columns pattern) of
+      Just { flipped: x /\ y, refl } /\ _ ->
+        refl * 100 + (fromMaybe 0 $ findReflection <<< Mx.columns <$> Mx.modify x y invert pattern)
+      _ /\ Just { flipped: y /\ x, refl } ->
+        refl + 100 * (fromMaybe 0 $ findReflection <<< Mx.rows <$> Mx.modify x y invert pattern)
+      _ -> 0
+  )
+  where
+    findSmudged slices = F.findMap reflectionPoint oneOffPairs
+      where
+        bound = Array.length slices - 1
+        matchedPairs = matchPairs slices
+        oneOffPairs = Array.mapWithIndex { i: _, v: _ } slices # pairs
+          # Array.mapMaybe \({ i: i1, v: v1 } /\ { i: i2, v: v2 }) -> case differences v1 v2 of
+            [i] -> Just { i, pair: i1 /\ i2 }
+            _ -> Nothing
+
+        reflectionPoint { i, pair: pair@(i1 /\ i2) } =
+          let refl = ((i2 - i1) `div` 2) + i1
+          in if F.all (\p -> p == pair || p `Set.member` matchedPairs) $ Array.zip (refl .. 0) ((refl + 1) .. bound)
+            then Just { flipped: i /\ i1, refl: refl + 1 } else Nothing
+
+differences :: forall a. Eq a => Array a -> Array a -> Array Int
+differences = (Array.catMaybes <<< Array.mapWithIndex \i t -> if t then Just i else Nothing) <.. Array.zipWith (/=)
+
 findReflection :: Array (Array Terrain) -> Int
-findReflection slices = fromMaybe 0 $ F.findMap reflectionPoint reflectedPairs
+findReflection slices = fromMaybe 0 $ F.findMap reflectionPoint matchedPairs
   where
     bound = Array.length slices - 1
-
-    reflectedPairs = Array.mapWithIndex (\i s -> Tuple s [i]) slices
-      # Map.fromFoldableWith (Array.sort <.. (<>))
-      # (Array.fromFoldable >=> pairs)
-      # Set.fromFoldable
-
+    matchedPairs = matchPairs slices
     reflectionPoint (Tuple i1 i2)
       | i1 == 0 || i2 == bound =
         let refl = ((i2 - i1) `div` 2) + i1
-        in if F.all (\i -> (Tuple i $ i1 + i2 - i) `Set.member` reflectedPairs) ((i1 + 1) ../ refl)
+        in if F.all (_ `Set.member` matchedPairs) $ Array.zip ((i1 + 1) .. refl) ((i2 - 1) .. (refl + 1))
           then Just $ refl + 1 else Nothing
       | otherwise = Nothing
 
-    pairs :: forall a. Array a -> Array (Tuple a a)
-    pairs xs = join $ Array.mapWithIndex (\i x -> Tuple x <$> Array.drop (i + 1) xs) xs
+matchPairs :: forall a. Ord a => Array a -> Set (Tuple Int Int)
+matchPairs = Array.mapWithIndex (\i s -> Tuple s [i])
+  >>> Map.fromFoldableWith (Array.sort <.. (<>))
+  >>> (Array.fromFoldable >=> pairs)
+  >>> Set.fromFoldable
+
+pairs :: forall a. Array a -> Array (Tuple a a)
+pairs xs = join $ Array.mapWithIndex (\i x -> Tuple x <$> Array.drop (i + 1) xs) xs
 
 parse :: String -> Either ParseError (Array Pattern)
 parse = String.split (S.Pattern "\n\n") >>> traverse (flip runParser pattern)
