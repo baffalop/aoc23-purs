788f73e9ad3da223d3ab8294daf42b166af4de19
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-25 22:32:07 +0000

    Implement Array.partitionMap (just in case)

--- src/Utils/Array.purs
+++ src/Utils/Array.purs
@@ -5,7 +5,9 @@ import PointFree ((<..))
 import Data.Array as Array
 import Data.Ordering (invert)
 import Data.Maybe (Maybe(..))
-import Data.Array ((..))
+import Data.Array ((..), (:))
+import Data.Either (Either(..))
+import Data.Foldable (foldr)
 
 sortDesc :: forall a. Ord a => Array a -> Array a
 sortDesc = Array.sortBy (invert <.. compare)
@@ -24,3 +26,11 @@ rangeAsc from to
   | otherwise = []
 
 infix 8 rangeAsc as ../
+
+partitionMap :: forall a b c. (a -> Either b c) -> Array a -> { left :: Array b, right :: Array c }
+partitionMap f = foldr
+  (\a res@{ left, right } -> case f a of
+    Left b -> res { left = b : left }
+    Right c -> res { right = c : right }
+  )
+  { left: [], right: [] }
