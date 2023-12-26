c60fd71b68c527bad3a6bed4e7f95f89aa1716ef
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-26 08:26:53 +0000

    Another attempt at 10.2

--- src/Day10.purs
+++ src/Day10.purs
@@ -12,57 +12,78 @@ import Data.Tuple.Nested ((/\))
 import Parsing.Combinators (choice, skipMany) as P
 import Parsing.String (char) as P
 import Parsing (position) as P
-import Data.Tuple (Tuple(Tuple), fst, snd)
+import Data.Tuple (Tuple(Tuple), fst, snd, uncurry)
 import Control.Bind (join)
 import Data.Either (Either)
 import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Either (note) as Either
 import Data.Array as Array
 import Data.Bifunctor (lmap)
-import Data.Array.NonEmpty (NonEmptyArray)
+import Data.Array.NonEmpty (NonEmptyArray, (!!))
 import Data.Array.NonEmpty as NA
 import Data.Foldable as F
 import Input (readInput)
 import Data.Set (Set)
 import Data.Set as Set
 import Utils.String (lines) as String
-import Data.Ord (between)
+import Data.Ord (abs, between)
 import Data.String (length) as String
 import Data.Foldable (class Foldable)
 import Uncurried.State (State)
 import Data.Lens.Record (prop) as L
 import Type.Proxy (Proxy(Proxy))
-import Data.Lens.Setter ((%~))
+import Data.Lens.Setter ((%~), (.=))
 import Control.Monad.State.Class (modify) as State
-import Data.Traversable (traverse)
-import Data.List (List(..), (:))
+import Data.List (List(..), (:), (\\))
 import Data.List as List
 import Data.Lens.Getter (use)
 import Utils.State (foldrState)
+import Control.Applicative (pure)
+import Utils.Basics (mapBoth)
+import Debug (spy, spyWith) as Debug
 
 type Coord = Tuple Int Int
 
 type FillState =
   { visited :: Set Coord
+  , from :: List Coord
   }
 
 solve1 :: String -> Either String Int
 solve1 s = do
   pipes <- lmap parseErrorMessage $ parse s
   loop <- findLoop pipes
-  pure $ Set.size loop `div` 2
+  pure $ NA.length loop `div` 2
 
-solve2 :: String -> Either String Int
 solve2 s = do
-  pipes <- lmap parseErrorMessage $ parse s
-  loop <- findLoop pipes
-  let
-    fillFrom :: Coord -> Set Coord -> State FillState (Set Coord)
-    fillFrom coord enclosed = do
-      branches <- traverse (fill Set.empty <<< List.singleton) $ neighbours coord
-      pure $ Set.union enclosed $ F.fold $ List.catMaybes branches
+  rawPipes <- lmap parseErrorMessage $ parse s
+  loop <- findLoop rawPipes
+  pipes <- Either.note "Couldn't fill starting pipe" do
+    let start = Debug.spy "start" $ NA.last loop
+    let end = NA.head loop
+    prev <- loop !! (NA.length loop - 2)
+    pure $ Map.insert start [prev, end] rawPipes
 
-    fill :: Set Coord -> List Coord -> State { visited :: Set Coord } (Maybe (Set Coord))
+  let
+    walk :: Coord -> Maybe (Set Coord) -> State FillState (Maybe (Set Coord))
+    walk _ Nothing = pure Nothing
+    walk coord (Just enclosed) = case Map.lookup coord pipes of
+      Nothing -> pure Nothing
+      Just passages -> do
+        from <- use _from
+        let
+          _ = Debug.spyWith "coord" show coord
+          _ = Debug.spyWith "from" show from
+--          _ = Debug.spyWith "passages" show passages
+          candidates = Debug.spyWith "candidates" show $ neighbours coord \\ List.fromFoldable passages
+          nearFrom c = F.any (adjacent c) from
+          toFill = Debug.spyWith "to fill" show if allAdjacent candidates && F.any nearFrom candidates
+            then candidates else List.filter nearFrom candidates
+        when (not $ List.null toFill) $ _from .= toFill
+        filled <- fill enclosed $ List.fromFoldable toFill
+        pure $ Debug.spyWith "filled" show filled
+
+    fill :: Set Coord -> List Coord -> State FillState (Maybe (Set Coord))
     fill enclosed Nil = pure $ Just enclosed
     fill enclosed (coord : candidates)
       | outOfBounds coord = pure Nothing
@@ -74,19 +95,28 @@ solve2 s = do
             $ List.filter (not <<< (_ `Set.member` visited))
             $ candidates <> neighbours coord
 
-  pure $ Set.size $ foldrState fillFrom { visited: loop } Set.empty loop
+  startingPoints <- Either.note "Can't prepare starting points" do
+    let first = NA.head loop
+    passages <- Map.lookup first pipes
+    pure $ neighbours first \\ List.fromFoldable passages
+
+  pure $ (\start -> foldrState walk { from: start : Nil, visited: Set.fromFoldable loop } (Just Set.empty) loop)
+    <$> startingPoints
   where
     lines = String.lines s
     colCount = fromMaybe 0 $ String.length <$> Array.head lines
     outOfBounds = not <<< between (1 /\ 1) (colCount /\ Array.length lines)
 
-findLoop :: Map Coord (Array Coord) -> Either String (Set Coord)
+    allAdjacent coords = F.all (\c -> F.all (adjacent c) coords) coords
+    adjacent c1 c2 = uncurry (&&) $ mapBoth (abs >>> (_ <= 1)) $ c1 - c2
+
+findLoop :: Map Coord (Array Coord) -> Either String (NonEmptyArray Coord)
 findLoop pipes = do
   start <- Either.note "No start found" $ fst <<$>> Array.find (Array.null <<< snd) $ Map.toUnfoldable pipes
   let
-    follow :: NonEmptyArray Coord -> Coord -> Maybe (Set Coord)
+    follow :: NonEmptyArray Coord -> Coord -> Maybe (NonEmptyArray Coord)
     follow visited to
-      | to == start = Just $ Set.fromFoldable visited
+      | to == start = Just visited
       | otherwise = case Array.filter (_ /= NA.head visited) <$> Map.lookup to pipes of
         Just [next] -> follow (to NA.: visited) next
         _ -> Nothing
@@ -97,6 +127,7 @@ neighbours :: Coord -> List Coord
 neighbours c = (c + _) <$> (north : south : east : west : Nil)
 
 _visited = L.prop (Proxy :: Proxy "visited")
+_from = L.prop (Proxy :: Proxy "from")
 
 parse :: String -> Either ParseError (Map Coord (Array Coord))
 parse s = runParser s $ Map.fromFoldable <<< join <<$>> linesOf $ P.many do
@@ -128,13 +159,13 @@ SJLL7
 |F--J
 LJ.LJ"""
 
-example2 = """.S------7.
-.|F----7|.
-.||OOOO||.
-.||OOOO||.
-.|L-7F-J|.
-.|II||II|.
-.L--JL--J."""
+example2 = """.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J."""
 
 example3 = """.F----7F7F7F7F-7....
 .|F--7||||||||FJ....
