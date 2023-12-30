84ab3f1169a822a8ea4a043c554947b43837d13e
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-30 21:47:58 +0000

    Parse day 16

---
+++ src/Day16.purs
@@ -0,0 +1,45 @@
+module Day16 where
+-- The Floor Will Be Lava
+
+import Prelude
+import Utils.Parsing (parseGrid)
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple)
+import Data.Map (Map)
+import Data.Map as Map
+
+data Optic
+  = MirrorNE
+  | MirrorNW
+  | SplitterV
+  | SplitterH
+
+derive instance eqOptic :: Eq Optic
+derive instance ordOptic :: Ord Optic
+instance showOptic :: Show Optic where
+  show = case _ of
+    MirrorNE -> "/"
+    MirrorNW -> "\\"
+    SplitterV -> "|"
+    SplitterH -> "-"
+
+type Coord = Tuple Int Int
+
+parse :: String -> Map Coord Optic
+parse = parseGrid $ case _ of
+  '|' -> Just SplitterV
+  '-' -> Just SplitterH
+  '\\' -> Just MirrorNW
+  '/' -> Just MirrorNE
+  _ -> Nothing
+
+example = """.|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|...."""
