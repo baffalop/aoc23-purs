97c1c219d22c5232b88d9c9342961a38d702d600
Nikita Gaidakov <ngaidakov@podfather.com>
2023-12-30 00:49:19 +0000

    Solve 14.1

--- src/Day14.purs
+++ src/Day14.purs
@@ -7,10 +7,14 @@ import Data.String.CodeUnits (toCharArray) as String
 import Data.FunctorWithIndex (mapWithIndex)
 import Data.Map (Map)
 import Data.Map as Map
-import Data.Tuple (Tuple(..))
+import Data.Tuple (Tuple(..), snd)
 import Data.Tuple.Nested ((/\))
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), maybe)
 import Data.Array (catMaybes) as Array
+import Data.Foldable as F
+import Data.FoldableWithIndex (foldlWithIndex)
+import Data.Set as Set
+import Input (readInput)
 
 data Rock = Round | Square
 
@@ -20,6 +24,33 @@ derive instance eqRock :: Eq Rock
 instance showRock :: Show Rock where
   show Round = "O"
   show Square = "#"
+
+solve1 :: String -> Int
+solve1 = parse >>> tiltRocks >>> totalLoad
+  where
+    tiltRocks :: Map Coord Rock -> Map Coord Rock
+    tiltRocks rocks = foldlWithIndex
+      (\coord@(x /\ y) { rocks, lastCoord: (lastx /\ lasty) } rock -> case rock of
+        Square -> { rocks, lastCoord: coord }
+        Round ->
+          let movedCoord = if x == lastx then x /\ (lasty + 1) else x /\ 0
+          in
+          { rocks: rocks # Map.delete coord # Map.insert movedCoord rock
+          , lastCoord: movedCoord
+          }
+      )
+      { lastCoord: 0 /\ -1, rocks }
+      rocks
+      # _.rocks
+
+    totalLoad :: Map Coord Rock -> Int
+    totalLoad rocks = F.sum
+      $ mapWithIndex (\(_ /\ y) -> case _ of
+        Square -> 0
+        Round -> rows - y
+      ) rocks
+      where
+        rows = Map.keys rocks # Set.map snd # Set.findMax # maybe 0 (_ + 1)
 
 parse :: String -> Map Coord Rock
 parse = String.lines
@@ -44,3 +75,5 @@ O.#..O.#.#
 .......O..
 #....###..
 #OO..#...."""
+
+input = readInput 14
